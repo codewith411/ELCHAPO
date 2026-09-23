342ed247684cb854b98a59e39883901066dd07bd
Richie Mwebi <CODEWITH411@GMAIL.COM>
2026-09-23 14:11:13 +0300

    Learn flask jinja

---
+++ FLASK/serverSideRendering/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, send_file, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Link AI model here
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
